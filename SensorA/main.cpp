c3bddff030f8a3853f1ff38743475ac69374ca0f
Sergei <maslennikovserge@yandex.ru>
2025-03-15 00:12:35 +0300

    namespace mqtt = utils::packet_MQTT;

--- SensorA/main.cpp
+++ SensorA/main.cpp
@@ -10,6 +10,7 @@
 #include <utilsPacketMQTT.h>
 
 using boost::asio::ip::tcp;
+namespace mqtt = utils::packet_MQTT;
 
 //std::size_t ReceivePacket(tcp::socket& socket, utils::packet_MQTT::tControlPacketType& packetType)
 //{
@@ -35,7 +36,7 @@ using boost::asio::ip::tcp;
 void TaskConnectHandler(tcp::socket& socket)
 //utils::packet_MQTT::tPacketCONNACK TaskConnectHandler(tcp::socket& socket)
 {
-	utils::packet_MQTT::tPacketCONNECT PackCONNECT(false, 10, "duper_star", utils::packet_MQTT::tQoS::AtLeastOnceDelivery, true, "SensorA_will", "something wrong has happened"); // 1883
+	mqtt::tPacketCONNECT PackCONNECT(false, 10, "duper_star", mqtt::tQoS::AtLeastOnceDelivery, true, "SensorA_will", "something wrong has happened"); // 1883
 
 	std::cout << PackCONNECT.ToString() << '\n';
 
@@ -49,17 +50,17 @@ void TaskConnectHandler(tcp::socket& socket)
 	if (!SizeRcv)
 		THROW_RUNTIME_ERROR("PREVED: NO DATA 1");
 
-	utils::packet_MQTT::tSpan Span(Buffer, SizeRcv);
-	auto Res = utils::packet_MQTT::TestPacket(Span);
+	mqtt::tSpan Span(Buffer, SizeRcv);
+	auto Res = mqtt::TestPacket(Span);
 	if (!Res.has_value())
 		THROW_RUNTIME_ERROR("PREVED: NO PACKET 1"); // Res.error() - put it into the message
 
-	Span = utils::packet_MQTT::tSpan(Buffer, SizeRcv);
+	Span = mqtt::tSpan(Buffer, SizeRcv);
 
 	if (*Res != utils::packet_MQTT::tControlPacketType::CONNACK)
 		THROW_RUNTIME_ERROR("PREVED: WRONG PACKET 1");
 
-	auto Pack_parsed = utils::packet_MQTT::tPacketCONNACK::Parse(Span);
+	auto Pack_parsed = mqtt::tPacketCONNACK::Parse(Span);
 	if (!Pack_parsed.has_value())
 		THROW_RUNTIME_ERROR("PREVED: WRONG PACKET 2"); // Res.error() - put it into the message
 
