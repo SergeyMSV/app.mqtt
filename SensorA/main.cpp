494179d6341d551231053e132bf25ddbafe27240
Sergei <maslennikovserge@yandex.ru>
2025-03-27 01:20:34 +0300

    SensorA + SensorData

--- SensorA/main.cpp
+++ SensorA/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 
+#include <format>
 #include <future>
 #include <iostream>
 #include <optional>
@@ -18,20 +19,20 @@ void TaskConnectionHandler(tcp::socket& socket);
 
 int main()
 {
-	int ExitCode = utils::exit_code::EX_OK;
-
-	try
+	while (true)
 	{
-		utils::tMeasureDuration Measure("MAIN");
-		//for (;;)
+		try
 		{
+			utils::tMeasureDuration Measure("Sending measurements...");
+
 			boost::asio::io_context ioc;
 
 			tcp::resolver Resolver(ioc);
 			tcp::resolver::results_type Ep = Resolver.resolve("test.mosquitto.org", "1883"); // MQTT, unencrypted, unauthenticated
 			//tcp::resolver::results_type Ep = resolver.resolve("test.mosquitto.org", "1884"); // 1884 : MQTT, unencrypted, authenticated
-
 			tcp::socket Socket(ioc);
+
+			const std::string SensorData = std::format("{:%F %T}", floor<std::chrono::seconds>(std::chrono::system_clock::now()));
 
 			try
 			{
@@ -39,39 +40,6 @@ int main()
 
 				std::future<void>TaskConnectionFuture = std::async(std::launch::async, TaskConnectionHandler, std::ref(Socket));
 				//std::future<void>TaskConnectFuture = std::async(std::launch::deferred, TaskConnectHandler, std::ref(Socket)); // a task is not started by wait_for(..), it'll be deferred forever
-
-				//std::future<void>TaskMeasureFuture = std::async(std::launch::async, TaskMeasureHandler);
-
-				g_Log.TestMessage("SOME IMPORTANT WORK STARTED");
-				std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // some important work...
-				g_Log.TestMessage("SOME IMPORTANT WORK FINISHED");
-
-				//MeasureData = TaskMeasureFuture.get();
-
-				//TaskConnectFuture.wait();
-
-				std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // some important work...
-
-				//std::future_status Status;
-
-				//do
-				//{
-				//	Status = TaskConnectionFuture.wait_for(std::chrono::milliseconds(1));
-				//	switch (Status)
-				//	{
-				//	case std::future_status::deferred:
-				//		std::cout << "deferred\n";
-				//		break;
-				//	case std::future_status::timeout:
-				//		std::cout << "timeout\n";
-				//		break;
-				//	case std::future_status::ready:
-				//		std::cout << "ready!\n";
-				//		break;
-				//	}
-				//} while (Status != std::future_status::ready);
-
-				g_Log.TestMessage("SOME NOT IMPORTANT WORK");
 
 				TaskConnectionFuture.get();
 			}
@@ -81,20 +49,17 @@ int main()
 			}
 
 			Socket.close();
-
-			g_Log.Operation("WAIT");
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		}
+		catch (std::exception& ex)
+		{
+			g_Log.Exception(ex.what());
+
+			//ExitCode = utils::exit_code::EX_IOERR;
+		}
+
+		utils::tMeasureDuration Measure("Sleeping...");
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // [#] pause - it can be in the settings
 	}
-	catch (std::exception& ex)
-	{
-		g_Log.Exception(ex.what());
 
-		// g_DataSetMainControl.Thread_GNSS_State = tDataSetMainControl::tStateGNSS::Exit; notify all the threads that it's time to exit
-
-		ExitCode = utils::exit_code::EX_IOERR;
-	}
-
-	return ExitCode;
+	return utils::exit_code::EX_OK;
 }
